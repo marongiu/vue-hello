5ff71a506c47bdbe5070dc496d45704def9493d4
Andrea <andreamarongiu05@gmail.com>
2021-03-23 15:03:10 +0100

    Fine esercizio base

---
+++ main.js
@@ -0,0 +1,10 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+  // inizializzo vue
+new Vue({
+    // lo posiziono nell'html
+  el: '#root',
+  data: {
+    esercizioBase: 'Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.',
+    message: 'Hello Vue.js!',
+  }
+})
