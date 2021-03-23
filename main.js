82705d664cc18099de057f3425376e0a429642b7
Andrea <andreamarongiu05@gmail.com>
2021-03-23 15:09:29 +0100

    Correzione sintassi

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
-  // inizializzo vue
-new Vue({
+// inizializzo vue
+var app = new Vue(
+  {
     // lo posiziono nell'html
-  el: '#root',
-  data: {
-    esercizioBase: 'Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.',
-    message: 'Hello Vue.js!',
+    el: '#root',
+    data: {
+      consegna: "Stampare a schermo un messaggio all’interno di un h1",
+      esercizioBase: "Hello Vue.js!"
+      // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+    }
   }
-})
+);
