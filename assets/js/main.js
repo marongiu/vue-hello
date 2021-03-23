e2941dc1d260208c5448374b2a4989b37e0e753b
Andrea <andreamarongiu05@gmail.com>
2021-03-23 15:47:54 +0100

    Bonus input

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,15 @@ var app = new Vue(
     // lo posiziono nell'html
     el: '#root',
     data: {
-      consegna: "Stampare a schermo un messaggio all’interno di un h1",
       esercizioBase: "Hello Vue.js!",
       // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
         // immagine con url
       immagineUrl: "https://velug.it/images/banners/JQUERYLOGO.gif",
         // immagine importata
-      immagine: "./assets/img/vue.png"
+      immagine: "./assets/img/vue.png",
+
+      ///////////////BONUS//////////////////////////////
+      cambia: "Cambia testo al titolo!",
     }
   }
 );
